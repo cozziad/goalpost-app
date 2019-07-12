95d2317de20677f4b288af5a40786f3426152e7b
Anthony <cozziad@gmail.com>
2019-07-12 11:19:13 -0400

    Initial Commit

---
+++ goalpost-app/Controller/GoalsVC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by Anthony Cozzi on 7/12/19.
+//  Copyright © 2019 Anthony Cozzi. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    //outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addGoalPressed(_ sender: Any) {
+    }
+    
+}
+
